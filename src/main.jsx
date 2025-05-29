0dc579651149ec63209f23cb3da837c7747c4273
Muaz Hussain <muaz.self@gmail.com>
2025-05-30 01:57:19 +0600

    Use Fragment - v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,40 @@
 import { createRoot } from "react-dom/client";
+import { Fragment } from "react";
 
-createRoot(document.getElementById("root")).render(<WhyIAmLearningReact />);
+const root = createRoot(document.getElementById("root"));
 
-function WhyIAmLearningReact() {
+root.render(
+  <Fragment>
+    <Header />
+    <Body />
+    <Footer />
+  </Fragment>
+);
+
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react.svg" alt="" width="80px" />
+    </header>
+  );
+}
+
+function Body() {
   return (
     <main>
-      <h3>Why I am learning React: </h3>
+      <h1>Reasons I'm excited to learn react</h1>
       <ol>
-        <li>For handling monitoring tool</li>
-        <li>For becoming a true full stack engineer</li>
-        <li>Make me more an independent person</li>
+        <li>It's very popular framework</li>
+        <li>It gives an easier way to build frontend for web application</li>
       </ol>
     </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>All rights reserved.</p>
+    </footer>
   );
 }
