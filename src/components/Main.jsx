bc0ad7f703ef8918b3ae3161a371c49e035c47df
Muaz Hussain <muaz.self@gmail.com>
2025-06-15 02:21:24 +0600

    Challenge

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+  return (
+    <main>
+      <h1>Fun facts about react!</h1>
+      <ul className="facts-list">
+        <li>Was released in 2013</li>
+        <li>Was orizinally created by Jordan Walke</li>
+        <li>Has well over 200K stars on Github</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+}
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+export default function NavBar() {
+  return (
+    <header>
+      <nav>
+        <img src="/src/assets/react.svg" alt="react-svg" />
+        <span>ReactFacts</span>
+      </nav>
+    </header>
+  );
+}
